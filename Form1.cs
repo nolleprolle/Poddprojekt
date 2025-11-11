1ae8dc23141df1b78efc0a371635f990465042c7
linahanssons <hanssonlinas@gmail.com>
2025-11-11 14:03:21 +0100

    Databas kontakt

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoddprojektetOru
+
+{
+    public partial class Form1 : Form
+    {
+        private MongoDBService _mongoService;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _mongoService = new MongoDBService();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool connected = _mongoService.TestConnection();
+
+            if (connected)
+                MessageBox.Show("✅ Anslutningen till MongoDB lyckades!");
+            else
+                MessageBox.Show("❌ Misslyckades med att ansluta till MongoDB.");
+        }
+    }
+}
